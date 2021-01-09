1743d9969a0dc8bf835d4da2ec78efbdf3011659
jasonwei0224 <45651276+jasonwei0224@users.noreply.github.com>
2021-01-08 16:54:20 -0800

    udpate swagger ui documentation

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,54 @@ const express = require('express');
 const router = express.Router();
 const userModel = require('../models/user')
 
+/**
+ * @swagger
+ * /user/{email}:
+ *   get:
+ *     tags:
+ *       - User
+ *     description: Get User info
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: email
+ *         in: path
+ *         require: true
+ *         type: string
+ *         example: test@mail.com
+ *     responses:
+ *       200:
+ *          description: success get user info
+ *          content:
+ *             application/json:
+ *              example:
+ *                  [
+ *                   {
+ *                      message: success get user email,
+ *                      data: [
+ *                              {
+ *                                  "restaurantOwned": [],
+ *                                  "_id": "5asdfasdfoipje",
+ *                                  "email": "test@gmail.com",
+ *                                  "firstName": "J",
+ *                                  "lastName": "Adam",
+ *                                  "userType": "Admin",
+ *                                  "address": "123456 211",
+ *                                  "city": "Vancouver",
+ *                                   "__v": 0
+ *                      }]
+ *       
+ *                   }
+ *               ]
+ *       400:
+ *          description: Unauthorized
+ *       default:
+ *         description: unable to retrieve data
+ *     security:
+ *       - Secured: []
+ */
 router.get('/:email', function(req, res, next) {
     userModel.aggregate([{
                 $match: { email: req.params.email }
@@ -26,11 +74,46 @@ router.get('/:email', function(req, res, next) {
                 res.status(500).json({ message: "unable to retrieve data" });
             }
             // console.log(JSON.stringify(result, undefined, 2))
-            res.status(200).json({ message: "success get user email", data: result });
+            res.status(200).json({ message: "success get user info", data: result });
         })
 
 })
 
+/**
+ * @swagger
+ * /user/{email}:
+ *   delete:
+ *     tags:
+ *       - User
+ *     description: delete user
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: term
+ *         in: path
+ *         require: true
+ *         type: string
+ *         example: test@mail.com
+ *     responses:
+ *       200:
+ *          description: OK
+ *          content:
+ *             application/json:
+ *              example:
+ *                  [
+ *                   {
+ *                      message: success delete user info
+ *                   }
+ *               ]
+ *       400:
+ *          description: Fail to add 
+ *       default:
+ *         description: Fail to delete
+ *     security:
+ *       - Secured: []
+ */
 router.delete('/:email', function(req, res, next) {
     userModel.deleteOne({ email: req.params.email }).then((result) => {
         if (result.deleteCount < 1) {
@@ -41,6 +124,82 @@ router.delete('/:email', function(req, res, next) {
     })
 })
 
+/**
+ * @swagger
+ * /user/:
+ *   post:
+ *     tags:
+ *       - User
+ *     description: Add New User
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: email
+ *         in: body
+ *         require: true
+ *         type: string
+ *         example: test@mail.com
+ *       - name: firstName
+ *         in: body
+ *         require: true
+ *         type: string
+ *         example: Yun
+ *       - name: lastName
+ *         in: body
+ *         require: true
+ *         type: string
+ *         example: Sangwook
+ *       - name: userType
+ *         in: body
+ *         require: true
+ *         type: string
+ *         example: Admin
+ *       - name: address
+ *         in: body
+ *         require: true
+ *         type: string
+ *         example: 1234 244 st
+ *       - name: city
+ *         in: body
+ *         require: true
+ *         type: string
+ *         example: Vancouver
+ *       - name: province
+ *         in: body
+ *         require: true
+ *         type: string
+ *         example: BC
+ *     responses:
+ *       201:
+ *          description: success post user info
+ *          content:
+ *             application/json:
+ *              example:
+ *                  [
+ *                   {
+ *                      message: success post user info",
+ *                      data: [{
+ *                                  "restaurantOwned": [],
+ *                                  "_id": "5asdfasdfoipje",
+ *                                  "email": "test@gmail.com",
+ *                                  "firstName": "J",
+ *                                  "lastName": "Adam",
+ *                                  "userType": "Admin",
+ *                                  "address": "123456 211",
+ *                                  "city": "Vancouver",
+ *                                   "__v": 0
+ *                      }]
+ *                   }
+ *               ]
+ *       400:
+ *          description: Unauthorized 
+ *       default:
+ *         description: fail to create use
+ *     security:
+ *       - Secured: []
+ */
 router.post('/', function(req, res, next) {
 
     userModel.create({
@@ -50,7 +209,7 @@ router.post('/', function(req, res, next) {
         userType: req.body.userType,
         address: req.body.address,
         city: req.body.city,
-        provice: req.body.city,
+        provice: req.body.province,
     }).then((result) => {
         res.status(201).json({ message: "success post user info", data: result });
     }).catch((e) => {
@@ -59,6 +218,82 @@ router.post('/', function(req, res, next) {
     })
 })
 
+/**
+ * @swagger
+ * /user/:
+ *   patch:
+ *     tags:
+ *       - User
+ *     description: Update User
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: email
+ *         in: body
+ *         require: true
+ *         type: string
+ *         example: test@mail.com
+ *       - name: firstName
+ *         in: body
+ *         require: true
+ *         type: string
+ *         example: Yun
+ *       - name: lastName
+ *         in: body
+ *         require: true
+ *         type: string
+ *         example: Sangwook
+ *       - name: userType
+ *         in: body
+ *         require: true
+ *         type: string
+ *         example: Admin
+ *       - name: address
+ *         in: body
+ *         require: true
+ *         type: string
+ *         example: 1234 244 st
+ *       - name: city
+ *         in: body
+ *         require: true
+ *         type: string
+ *         example: Vancouver
+ *       - name: province
+ *         in: body
+ *         require: true
+ *         type: string
+ *         example: BC
+ *     responses:
+ *       201:
+ *          description: success post user info
+ *          content:
+ *             application/json:
+ *              example:
+ *                  [
+ *                   {
+ *                      message: success post user info",
+ *                      data: [{
+ *                                  "restaurantOwned": [],
+ *                                  "_id": "5asdfasdfoipje",
+ *                                  "email": "test@gmail.com",
+ *                                  "firstName": "J",
+ *                                  "lastName": "Adam",
+ *                                  "userType": "Admin",
+ *                                  "address": "123456 211",
+ *                                  "city": "Vancouver",
+ *                                   "__v": 0
+ *                      }]
+ *                   }
+ *               ]
+ *       400:
+ *          description: Unauthorized 
+ *       default:
+ *         description: fail to create use
+ *     security:
+ *       - Secured: []
+ */
 router.patch('/', function(req, res, next) {
     // console.log(req.body)
     userModel.findOneAndUpdate({ _id: req.body.userid }, {
@@ -74,7 +309,7 @@ router.patch('/', function(req, res, next) {
         res.status(200).json({ message: "success update user info", data: result });
 
     }).catch((e) => {
-        res.status(500).json({ message: "fail to updae user", error: err.errors });
+        res.status(500).json({ message: "fail to update user", error: err.errors });
     })
 
 })
