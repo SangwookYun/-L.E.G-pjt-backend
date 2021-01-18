c5aee1f10732d5513081dcea38e963bb79bf52ee
jasonwei0224 <45651276+jasonwei0224@users.noreply.github.com>
2021-01-18 12:10:23 -0800

    update firebase credentials

--- firebaseSetUp.js
+++ firebaseSetUp.js
@@ -1,9 +1,14 @@
 var admin = require("firebase-admin");
-
-var serviceAccount = require("./legbackend-firebase-adminsdk-nkepd-242f3e430b.json")
+const dotenv = require('dotenv');
+dotenv.config();
+// var serviceAccount = require("./legbackend-firebase-adminsdk-nkepd-242f3e430b.json")
 
 var app = admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+        "private_key": process.env.private_key.replace(/\\n/g, '\n'),
+        "client_email": process.env.client_email,
+        "project_id": process.env.project_id,
+    }),
     databaseURL: "https://legbackend-default-rtdb.firebaseio.com"
 })
 
