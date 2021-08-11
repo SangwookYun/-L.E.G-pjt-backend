e2c12c3a748aabc2a0cff8ddab925857acac5c5d
Hendrik Kim <48870515+hendrik-kim@users.noreply.github.com>
2021-08-10 20:38:04 -0500

    edit server excute quote

--- server.js
+++ server.js
@@ -1,50 +1,51 @@
-const express = require('express')
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const PORT = process.env.PORT || 8080; // Need this for Heroku
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-const restaurant = require('./routes/restaurant')
-const auth = require('./routes/auth');
-const advertisement = require('./routes/advertisement')
-const category = require('./routes/category')
-const coupon = require('./routes/coupon')
-const mongoose = require('mongoose');
+const restaurant = require("./routes/restaurant");
+const auth = require("./routes/auth");
+const advertisement = require("./routes/advertisement");
+const category = require("./routes/category");
+const coupon = require("./routes/coupon");
+const mongoose = require("mongoose");
 
-const app = express()
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.options('*', cors());
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.options("*", cors());
 app.use(cors());
 app.use(express.json());
 
-const USERNAME = process.env.DBUSERNAME
-const DBPASSWORD = process.env.DBPASSWORD
-const DBNAME = process.env.DBNAME
+const USERNAME = process.env.DBUSERNAME;
+const DBPASSWORD = process.env.DBPASSWORD;
+const DBNAME = process.env.DBNAME;
 
 const uri = `mongodb+srv://${USERNAME}:${DBPASSWORD}@cluster0.l8dbx.mongodb.net/${DBNAME}?retryWrites=true&w=majority`;
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
-    (err) => {
-        if (err) throw err;
-        console.log('Successfully connected');
-    })
+mongoose.connect(
+  uri,
+  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
+  (err) => {
+    if (err) throw err;
+    console.log("Successfully connected");
+  }
+);
 
+const version = "/api/v1";
+app.use(version + "/auth", auth);
+app.use(version + "/restaurant", restaurant);
+app.use(version + "/advertisement", advertisement);
+app.use(version + "/category", category);
+app.use(version + "/coupon", coupon);
 
-const version = '/api/v1'
-app.use(version + '/auth', auth)
-app.use(version + '/restaurant', restaurant)
-app.use(version + '/advertisement', advertisement)
-app.use(version + '/category', category)
-app.use(version + '/coupon', coupon)
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
 
-app.use((error, req, res, next)=> {
-    console.log(error);
-    const status = error.statusCode ||500;
-    const message = error.message;
-    const data = error.data;
-    res.status(status).json({message: message, data:data})
-})
-
-
-app.listen(PORT, () => console.log("Listening..."))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
