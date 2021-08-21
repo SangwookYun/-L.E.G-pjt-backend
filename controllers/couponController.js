cd53ae43da4958ed81fa2996147a52801e5eed06
Hendrik Kim <48870515+hendrik-kim@users.noreply.github.com>
2021-08-21 05:11:53 -0500

    create coupon controller

---
+++ controllers/couponController.js
@@ -0,0 +1,60 @@
+import asyncHandler from "express-async-handler";
+import genToken from "../utils/genToken.js";
+import User from "../models/userModel.js";
+import Coupon from "./../models/couponModel.js";
+import CryptoJS from "crypto-js";
+
+const scanQrCode = asyncHandler(async (req, res) => {
+  let data = {
+    userId: "611357459555a9266cf0d811",
+    couponId: "60d7a7e3326c816e20d1ec4b",
+  };
+  let ciphertext = CryptoJS.AES.encrypt(
+    JSON.stringify(data),
+    "secret key 123"
+  ).toString();
+
+  const bytes = CryptoJS.AES.decrypt(ciphertext, "secret key 123");
+  //TODO:HENDRIK:Remove the above code after checking the datatype and type passed in from the FE.
+
+  const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+  const user = await User.findById(decryptedData.userId);
+  const coupon = await Coupon.findById(decryptedData.couponId);
+
+  if (
+    user &&
+    coupon &&
+    coupon.isUsed === false &&
+    user.coupons.includes(coupon._id)
+  ) {
+    let updatedUserCoupons = user.coupons.filter(function (e) {
+      return e._id.toString() !== coupon._id.toString();
+    });
+    user.coupons = updatedUserCoupons;
+  } else {
+    res.status(404);
+    throw new Error("Please check coupon information.");
+  }
+
+  const updatedUser = await user.save();
+  const updatedCoupon = await Coupon.findOneAndUpdate(
+    { _id: coupon._id },
+    { $set: { isUsed: true } },
+    { useFindAndModify: false },
+    (err) => {
+      if (err) {
+        err.reason = "Failed to update coupon status.";
+        res.status(404).send(err);
+      }
+    }
+  );
+
+  res.json({
+    _id: updatedUser._id,
+    userCoupons: updatedUser.coupons,
+    redeemedCoupon: { id: updatedCoupon._id, isUsed: updatedCoupon.isUsed },
+    token: genToken(updatedUser._id),
+  });
+});
+
+export { scanQrCode };
