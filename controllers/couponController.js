d1a91d70c2f8480e6ff7164708884e41455ba4f5
Hendrik Kim <48870515+hendrik-kim@users.noreply.github.com>
2021-09-12 00:13:23 -0500

    update scanned QR code function

--- controllers/couponController.js
+++ controllers/couponController.js
@@ -5,18 +5,7 @@ import Coupon from "./../models/couponModel.js";
 import CryptoJS from "crypto-js";
 
 const scanQrCode = asyncHandler(async (req, res) => {
-  let data = {
-    userId: "60d76260816ee225c09cc977",
-    couponId: "60d7a7e3326c816e20d1ec4b",
-  };
-  let ciphertext = CryptoJS.AES.encrypt(
-    JSON.stringify(data),
-    "secret key 123"
-  ).toString();
-
-  const bytes = CryptoJS.AES.decrypt(ciphertext, "secret key 123");
-  //TODO:HENDRIK:Remove the above code after checking the datatype and type passed in from the FE.
-
+  const bytes = CryptoJS.AES.decrypt(req.body, process.env.COUPON_SECRET);
   const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
   const user = await User.findById(decryptedData.userId);
   const coupon = await Coupon.findById(decryptedData.couponId);
